e59631462176d8f3e52a9f28e76141993b4b9642
smb193 <smb193@zips.uakron.edu>
2014-05-26 02:41:54 -0500

    Allow unreleased moves

---
+++ mods/stabmons/moves.js
@@ -0,0 +1,26 @@
+exports.Battlemovedex = {
+        diamondstorm: {
+                inherit: true,
+                isUnreleased: false
+        },
+        hyperspacehole: {
+                inherit: true,
+                isUnreleased: false
+        },
+        lightofruin: {
+                inherit: true,
+                isUnreleased: false
+        },
+        steameruption: {
+                inherit: true,
+                isUnreleased: false
+        },
+        thousandarrows: {
+                inherit: true,
+                isUnreleased: false
+        },
+        thousandwaves: {
+                inherit: true,
+                isUnreleased: false
+        }
+};
